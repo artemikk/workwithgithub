7346961801159b7ba00faed6ee05f150cab58710
Иван петров <has592@yandex.ru>
2021-05-24 17:50:23 +0300

    21

--- Bank/BankAccount.cs
+++ Bank/BankAccount.cs
@@ -45,10 +45,10 @@ namespace BankAccountNS
         }
         public static void Main()
         {
-            BankAccount ba = new BankAccount("Mr. Bryan Walton", 11.99);
+            BankAccount ba = new BankAccount("MR. Bryan Walton", 11.99);
             ba.Credit(5.77);
             ba.Debit(11.22);
-            Console.WriteLine("Current balance is ${0}", ba.Balance);
+            Console.WriteLine(" current balance is $ {0}", ba.Balance);
         }
     }
 }

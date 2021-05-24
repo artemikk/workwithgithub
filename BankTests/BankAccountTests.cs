b5cb502d57352fda961e5b9f7f41c0893b62cff8
Артём Хрущенко <has9876@outlook.com>
2021-05-24 17:37:17 +0300

    First

---
+++ BankTests/BankAccountTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BankAccountNS;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankAccountNS.Tests
+{
+    [TestClass()]
+    public class BankAccountTests
+    {
+        [TestMethod()]
+        public void BankAccountTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DebitTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CreditTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void MainTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
